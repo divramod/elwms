26d32bdbace24d9907fb5650f37a859cc356080e
divramod <divramod@gmail.de>
2015-09-02 00:45:34 +0200

    adapted to getCommonTasks

--- global.js
+++ global.js
@@ -1,10 +1,3 @@
-var colors = require("colors");
-var co = require("co");
-require("shelljs/global");
-var jobs = {};
-var result = {};
-var module_path = __dirname;
-
 // =========== [ job.index() ] ===========
 jobs.index = co.wrap(function*() {
     try {
@@ -12,54 +5,30 @@ jobs.index = co.wrap(function*() {
         // =========== [ get params from user input ] ===========
         var argv2 = process.env.dmnJob || process.argv[2] || "help";
 
-        // =========== [ help ] ===========
-        if (["help", "-help", "h", "-h"].indexOf(argv2) > -1) {
-            var task = require("./tasks/help/index.js");
-            yield task.start(module_path);
-        }
         // =========== [ test ] ===========
-        else if (["test", "-test", "t", "-t"].indexOf(argv2) > -1) {
+        if (["test", "-test", "t", "-t"].indexOf(argv2) > -1) {
             var task = require("./tasks/test/index.js");
             yield task.start();
         }
-        // =========== [ test ] ===========
-        else if (["todo"].indexOf(argv2) > -1) {
-            require("dm-npm").todo(__dirname);
-        }
-        // =========== [ prompt ] ===========
-        else if (["prompt","p"].indexOf(argv2) > -1) {
-            require("dm-npm").prompt(__dirname);
-        }
-        // =========== [ test ] ===========
-        else if (["idea"].indexOf(argv2) > -1) {
-            require("dm-npm").idea(__dirname);
-        }
-
         // =========== [ init project ] ===========
         else if (["project", "-project", "pr", "-pr"].indexOf(argv2) > -1) {
             var job = require("./modules/project/index.js");
             yield job.start(module_path);
         }
 
-        // =========== [ prompt ] ===========
-        else if (["prompt", "-prompt", "p", "-p"].indexOf(argv2) > -1) {
-            var job = require("./tasks/prompt/index.js");
-            yield job.start(module_path);
-        }
-
         // automatically add tasks here
 
-        // =========== [ help ] ===========
+
+        // =========== [ getCommonTasks ] ===========
         else {
-            var task = require("./tasks/help/index.js");
-            yield task.start(module_path);
+            require("dm-npm").getCommonTasks(argv2, __dirname);
         }
 
     } catch (e) {
         console.log("Filename: ", __filename, "\n", e.stack);
     }
 
-    return Promise.resolve(result);
+    return Promise.resolve();
 }); // job.index()
 
 // =========== [ MODULE EXPORT ] ===========
